6b02cb5d798d5a6d9a5b6b21779aeb9f2c8a1677
AlakJudge <davi.j.robson@gmail.com>
2025-05-06 16:46:22 +0100

    Added method to convert date and time to unix timestamp format
    Added the unix_convert_date_time method here, so it can be used anywhere with more ease

--- t_utils.py
+++ t_utils.py
@@ -1,5 +1,6 @@
 import discord
 from tournament import Tournament
+from datetime import datetime
 
 # Check if the user has the admin role or is a server admin
 async def check_tournament_admin(interaction: discord.Interaction, tournament: Tournament):          
@@ -22,3 +23,27 @@ async def move_reserve_to_player(tournament: Tournament):
         tournament.save()
         return True
     return False
+
+# Convert date and time to a Discord timestamp format and save it
+async def unix_convert_date_time(interaction, date: str, time: str) -> str:
+    # Validate date (DD/MM/YYYY format)
+    try:
+        parsed_date = datetime.strptime(date, "%d/%m/%Y")
+        if parsed_date <= datetime.now():
+            await interaction.response.send_message("The date has to be in the future. Please try again.", ephemeral=True)
+            return
+    except ValueError:
+        await interaction.response.send_message("Invalid date format. Please use 'DD/MM/YYYY'.", ephemeral=True)
+        return
+    
+    # Validate time (24-hour format)
+    try:
+        parsed_time = datetime.strptime(time, "%H:%M")  # 24-hour format
+    except ValueError:
+        await interaction.response.send_message("Invalid time format. Please use HH:MM (24-hour format).", ephemeral=True)
+        return
+
+    combined_datetime = datetime.combine(parsed_date.date(), parsed_time.time())
+    unix_timestamp = int(combined_datetime.timestamp()) # Convert to Unix timestamp
+    formatted_date_time = f"<t:{unix_timestamp}:F>" # Format: <t:unix_timestamp:F> for full date and time
+    return formatted_date_time
