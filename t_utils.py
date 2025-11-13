319a2e0e501b4c9469fae6595cb5a7f64061c746
AlakJudge <davi.j.robson@gmail.com>
2025-11-13 21:27:34 +0000

    Update t_utils.py

--- t_utils.py
+++ t_utils.py
@@ -170,6 +170,7 @@ async def send_notification(tournament: Tournament, interaction: discord.Interac
         return
     elif type == "checkin_end":
         tournament.checkin["ended"] = True
+        tournament.save()
         await move_players_to_reserve(tournament, type="end_of_checkin")
     else:
         message = f"## 🚨 REMINDER {participant_role.mention} 🚨: The tournament '{tournament.name}' will begin in {interval}!"
